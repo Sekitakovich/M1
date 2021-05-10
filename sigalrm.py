aca34e94b178a98ab56acf267fb940f97dd68fd8
k-seki <seki@magnetic.co.jp>
2021-05-11 08:03:26 +0900

    checking SIGALRM

--- sigalrm.py
+++ sigalrm.py
@@ -1,24 +1,28 @@
-from loguru import logger
 import signal
 from threading import Event
+from datetime import datetime as dt
+from loguru import logger
 
 
 class TimeKeeper(object):
-    def __init__(self, *, intervalSecs: int = 1):
+    def __init__(self, *, intervalSecs: float = 1):
         self.counter = 0
         self.intervalSecs = intervalSecs
         self.ev = Event()
+        self.top = dt.now()
+        self.now = self.top
         signal.signal(signal.SIGALRM, self.handler)
         signal.setitimer(signal.ITIMER_REAL, self.intervalSecs, self.intervalSecs)
 
     def handler(self, number, stack):
         self.counter += 1
+        self.now = dt.now()
         self.ev.set()
 
 
 if __name__ == '__main__':
     def main():
-        tk = TimeKeeper()
+        tk = TimeKeeper(intervalSecs=0.1)
         while True:
             try:
                 tk.ev.wait()
@@ -27,7 +31,7 @@ if __name__ == '__main__':
                 break
             else:
                 tk.ev.clear()
-                logger.info(f'counter = {tk.counter}')
+                logger.info(f'[{tk.counter}] {tk.now}')
 
 
     main()
